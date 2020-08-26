88cd78270937263017c83d890b6d66f3d1718f88
gamingtrex <50985287+gamingtrex@users.noreply.github.com>
2020-08-26 16:02:57 +0530

    .l,kmjhngb

--- meme.js
+++ meme.js
@@ -1,20 +1,14 @@
 const Discord = require('discord.js');
 const redditFetch = require('reddit-fetch');
-module.exports = {
-    name: "meme",
-    description: "meme",
-    execute(message, args) {
-        redditFetch({
+redditFetch({
 
-            subreddit: 'meme',
-            sort: 'new',
-            allowNSFW: true,
-            allowModPost: true,
-            allowCrossPost: true,
-            
-            }).then(post => {
-                message.channel.send(`Your meme from r/meme: ${post.url}`)
-                message.channel.send(`pp!help_reddit for more subreddits`);
-            });
-    }
-}
+    subreddit: 'meme',
+    sort: 'new',
+    allowNSFW: true,
+    allowModPost: true,
+    allowCrossPost: true,
+    
+    }).then(post => {
+        message.channel.send(`Your meme from r/meme: ${post.url}`)
+        message.channel.send(`pp!help_reddit for more subreddits`);
+    });
